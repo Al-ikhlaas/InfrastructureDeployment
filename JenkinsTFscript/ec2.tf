b106576d302506270838a96ab9246d6f30d2bcd3
Al-ikhlaas <167618896+Al-ikhlaas@users.noreply.github.com>
2024-11-12 23:28:48 +0100

    Update ec2.tf

--- JenkinsTFscript/ec2.tf
+++ JenkinsTFscript/ec2.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region    = "us-east-1"
-  profile   = "yusuf"
+  profile   = "Oyakhire"
 }
 
 
@@ -27,7 +27,6 @@ resource "aws_default_subnet" "default_az1" {
   }
 }
 
-
 # create security group for the ec2 instance
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
@@ -35,6 +34,22 @@ resource "aws_security_group" "ec2_security_group" {
   vpc_id      = aws_default_vpc.default_vpc.id
 
   # allow access on port 8080
+  ingress  {
+    description      = "http"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "https"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   ingress {
     description      = "http proxy access"
     from_port        = 8080
@@ -89,7 +104,9 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.small"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "devopskeypair"
+  key_name               = "Abdulkp"
+  user_data            = "${file("install_jenkins.sh")}"
+
 
   tags = {
     Name = "jenkins_server"
@@ -97,37 +114,40 @@ resource "aws_instance" "ec2_instance" {
 }
 
 
+
 # an empty resource block
-resource "null_resource" "name" {
+# resource "null_resource" "name" {
 
-  # ssh into the ec2 instance 
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file("~/Downloads/devopskeypair.pem")
-    host        = aws_instance.ec2_instance.public_ip
-  }
+#   # ssh into the ec2 instance 
+#   connection {
+#     type        = "ssh"
+#     user        = "ubuntu"
+#     private_key = file("~/Downloads/Abdulkp.pem")
+#     host        = aws_instance.ec2_instance.public_ip
+#   }
 
-  # copy the install_jenkins.sh file from your computer to the ec2 instance 
-  provisioner "file" {
-    source      = "install_jenkins.sh"
-    destination = "/tmp/install_jenkins.sh"
-  }
+#   # copy the install_jenkins.sh file from your computer to the ec2 instance 
+#   provisioner "file" {
+#     source      = "install_jenkins.sh"
+#     destination = "/tmp/install_jenkins.sh"
+#   }
 
-  # set permissions and run the install_jenkins.sh file
-  provisioner "remote-exec" {
-    inline = [
-        "sudo chmod +x /tmp/install_jenkins.sh",
-        "sh /tmp/install_jenkins.sh",
-    ]
-  }
+#   # set permissions and run the install_jenkins.sh file
+#   provisioner "remote-exec" {
+#     inline = [
+#         "sudo chmod +x /tmp/install_jenkins.sh",
+#         "sh /tmp/install_jenkins.sh",
+#     ]
+#   }
 
-  # wait for ec2 to be created
-  depends_on = [aws_instance.ec2_instance]
-}
+#   # wait for ec2 to be created
+#   depends_on = [aws_instance.ec2_instance]
+# }
 
 
 # print the url of the jenkins server
+
+
 output "website_url" {
   value     = join ("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
 }
